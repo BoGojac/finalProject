1304fcb0f4a03d713df021ddae056b85d9845b65
Adugna@BoGojac <adugnaolanigeleta@gmail.com>
2025-02-25 23:04:31 +0300

    simple content addition on vanBar

--- online-voting-System/component/navBar.jsx
+++ online-voting-System/component/navBar.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div>
-            
+            <nav className=''>
+                <div><img src="#" alt="#" /></div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>How It Work</li>
+                        <li>Quick Action</li>
+                        <li>Contact Us</li>
+                        <li>About</li>
+                    </ul>
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </nav>
         </div>
     );
 }
