cdf4f0029d0fd3b02fb16e82bbcfc032fe7d15bb
Adugna@BoGojac <adugnaolanigeleta@gmail.com>
2025-02-26 09:26:53 +0300

    try to recover

--- online-voting-System/src/App.jsx
+++ online-voting-System/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from '../component/navBar'
+// import NavBar from '../component/navBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <NavBar/>
+    {/* <NavBar/> */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='bg-sky-700 font-bold '>Vite + React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
